b070a322f90c1f52aa28b969b46969965add48a0
shnibble <joshualpatterson@gmail.com>
2021-02-27 12:29:24 -0500

    Added react-helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Hero from '../components/hero'
 import Discord from '../components/discord'
 import News from '../components/news'
 
 const IndexPage = () => (
   <>
+    <Helmet>
+      <title>Outland Country Club</title>
+    </Helmet>
     <Hero />
     <Discord />
     <News condensed={true} limit={3} />
