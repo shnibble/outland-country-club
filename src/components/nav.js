ab57aae963eb910801e67c2bae1d7c28b90c5279
shnibble <joshualpatterson@gmail.com>
2021-03-01 17:58:04 -0500

    simplified nav component.

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,24 +6,21 @@ const Container = styled.nav`
     display: flex;
     flex-direction: row;
     justify-content: center;
-    padding: 5px;
-    grid-gap: 5px;
-    max-width: 900px;
-    margin: auto;
 `
 const StyledLink = styled(Link)`
     display: block;
-    font-size: 20px;
-    color: #7289da;
     text-decoration: none;
 `
 
-const Nav = () => (
-    <Container>
-        <StyledLink to='/'>Home</StyledLink>
-        <StyledLink to='/news'>News</StyledLink>
-        <StyledLink to='/events'>Events</StyledLink>
-        <StyledLink to='/members'>Members</StyledLink>
-    </Container>
-)
+const Nav = () => {
+    return (
+        <Container>
+            <StyledLink to='/'>Home</StyledLink>
+            <StyledLink to='/news'>News</StyledLink>
+            <StyledLink to='/events'>Events</StyledLink>
+            <StyledLink to='/members'>Members</StyledLink>
+        </Container>
+    )
+}
+
 export default Nav
